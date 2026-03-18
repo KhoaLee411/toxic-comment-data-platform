64f192454caa516e21801306945c312e258c5e6f
Khoa Lee <leekhoa7678@gmail.com>
2026-03-18 21:21:22 +0700

    feat: upload data to datalake

---
+++ utils/upload_data_to_datalake.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+
+from load_config_from_file import load_cfg
+from minio import Minio
+
+# CFG_FILE = "./configs/config.yaml"
+# Robust path for config file relative to this script
+BASE_DIR = Path(__file__).resolve().parent.parent
+CFG_FILE = BASE_DIR / "configs" / "config.yml"
+
+
+def upload_data_local_to_minio(minio_client, local_path, bucket_name, folder_name):
+    local_path = Path(local_path)
+
+    for local_file in local_path.rglob("*"):
+        if local_file.is_file():
+            remote_path = os.path.join(
+                folder_name, str(local_file.relative_to(local_path))
+            )
+              
+            minio_client.fput_object(bucket_name, remote_path, str(local_file))
+            print(f"📤 Uploaded {local_file} → {bucket_name}/{remote_path}")
+
+
+def main():
+    cfg = load_cfg(CFG_FILE)
+    data_lake_cfg = cfg["data_lake"]
+    data_local_cfg = cfg["data_local"]
+
+    minio_client = Minio(
+        endpoint=data_lake_cfg["endpoint"],
+        access_key=data_lake_cfg["access_key"],
+        secret_key=data_lake_cfg["secret_key"],
+        secure=data_lake_cfg.get("secure", False),
+    )
+
+    if not minio_client.bucket_exists(data_lake_cfg["bucket_name"]):
+        minio_client.make_bucket(data_lake_cfg["bucket_name"])
+        print(f"✅ Created bucket: {data_lake_cfg['bucket_name']}")
+    else:
+        print(f"ℹ️ Bucket {data_lake_cfg['bucket_name']} already exists")
+
+    upload_data_local_to_minio(
+        minio_client,
+        data_local_cfg["local_path"],
+        data_lake_cfg["bucket_name"],
+        data_lake_cfg["folder_name"],
+    )
+
+
+if __name__ == "__main__":
+    main()
